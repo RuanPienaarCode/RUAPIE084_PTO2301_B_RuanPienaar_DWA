e913fd0ae78fc02a74b494be9181d2530d5c4848
Ruan Pienaar <ruan3d@gmail.com>
2023-08-29 23:17:35 +0200

    Add DWA11

--- DWA10/script.js
+++ DWA10/script.js
@@ -1,69 +1,131 @@
-// Select elements from the DOM
-const plusButton =
-	document.getElementById(
-		'plus'
-	);
-const minusButton =
-	document.getElementById(
-		'minus'
-	);
-const resetButton =
-	document.getElementById(
-		'reset'
-	);
-const counterElement =
-	document.getElementById(
-		'counter'
-	);
-// Initialize the counter variable
-let counter = 0;
-// Flag to control the reset display
-let showResetDisplay = false;
-// Update the counter element's text content
-function updateCounterDisplay() {
-	if (
-		showResetDisplay
-	) {
-		counterElement.textContent =
-			'Reset';
-		setTimeout(
+/**
+ * Represents a counter application that increments, decrements, and resets a counter.
+ */
+class CounterApp {
+	/**
+	 * Create a new CounterApp instance.
+	 * @constructor
+	 */
+	constructor() {
+		/** @type {number} The current counter value. */
+		this.counter = 0;
+
+		/** @type {boolean} Flag to control the reset display. */
+		this.showResetDisplay = false;
+
+		/** @type {HTMLElement} The plus button element. */
+		this.plusButton =
+			document.getElementById(
+				'plus'
+			);
+
+		/** @type {HTMLElement} The minus button element. */
+		this.minusButton =
+			document.getElementById(
+				'minus'
+			);
+
+		/** @type {HTMLElement} The reset button element. */
+		this.resetButton =
+			document.getElementById(
+				'reset'
+			);
+
+		/** @type {HTMLElement} The counter element. */
+		this.counterElement =
+			document.getElementById(
+				'counter'
+			);
+
+		// Attach event listeners to buttons
+		this.attachEventListeners();
+
+		// Update the initial counter display
+		this.updateCounterDisplay();
+	}
+
+	/**
+	 * Attach event listeners to the buttons.
+	 * @private
+	 */
+	attachEventListeners() {
+		/**
+		 * Event listener for the plus button click.
+		 */
+		this.plusButton.addEventListener(
+			'click',
 			() => {
-				counterElement.textContent =
-					'0';
-			},
-			1000
+				// Increment the counter value
+				this
+					.counter++;
+				// Update the counter display
+				this.updateCounterDisplay();
+			}
 		);
-	} else {
-		counterElement.textContent =
-			counter;
+
+		/**
+		 * Event listener for the minus button click.
+		 */
+		this.minusButton.addEventListener(
+			'click',
+			() => {
+				// Decrement the counter value if it's greater than 0
+				if (
+					this
+						.counter >
+					0
+				) {
+					this
+						.counter--;
+					// Update the counter display
+					this.updateCounterDisplay();
+				}
+			}
+		);
+
+		/**
+		 * Event listener for the reset button click.
+		 */
+		this.resetButton.addEventListener(
+			'click',
+			() => {
+				// Reset the counter value to 0 and show "Reset" briefly
+				this.counter = 0;
+				this.showResetDisplay = true;
+				// Update the counter display
+				this.updateCounterDisplay();
+			}
+		);
+	}
+
+	/**
+	 * Update the counter display based on the current counter value.
+	 * @private
+	 */
+	updateCounterDisplay() {
+		if (
+			this
+				.showResetDisplay
+		) {
+			// Display "Reset" temporarily before showing the counter value
+			this.counterElement.textContent =
+				'Reset';
+			setTimeout(
+				() => {
+					this.counterElement.textContent =
+						this.counter;
+				},
+				2000
+			);
+			this.showResetDisplay = false;
+		} else {
+			// Display the current counter value
+			this.counterElement.textContent =
+				this.counter;
+		}
 	}
 }
-// Event listener for the plus button
-plusButton.addEventListener(
-	'click',
-	() => {
-		counter++;
-		showResetDisplay = false;
-		updateCounterDisplay();
-	}
-);
-// Event listener for the minus button
-minusButton.addEventListener(
-	'click',
-	() => {
-		counter--;
-		showResetDisplay = false;
-		updateCounterDisplay();
-	}
-);
-// Event listener for the reset button
-resetButton.addEventListener(
-	'click',
-	() => {
-		counter = 0;
-		showResetDisplay = true;
-		updateCounterDisplay();
-	}
-);
-// Initial counter display
-updateCounterDisplay();
+
+// Create an instance of the CounterApp class
+const counterApp =
+	new CounterApp();
---
+++ DWA11/script.js
@@ -0,0 +1,207 @@
+/**
+ * Represents a Redux-inspired store for managing the counter state.
+ */
+class Store {
+	/**
+	 * Create a new Store instance.
+	 * @constructor
+	 * @param {Object} initialState - The initial state of the store.
+	 */
+	constructor(
+		initialState = {}
+	) {
+		this.state =
+			initialState;
+		this.subscribers =
+			[];
+	}
+
+	/**
+	 * Get the current state of the store.
+	 * @returns {Object} The current state.
+	 */
+	getState() {
+		return {
+			...this.state,
+		};
+	}
+
+	/**
+	 * Dispatch an action to update the state.
+	 * @param {Object} action - The action object with a "type" property.
+	 */
+	dispatch(action) {
+		this.state =
+			this.reducer(
+				this.state,
+				action
+			);
+		this.notifySubscribers();
+	}
+
+	/**
+	 * Subscribe a callback function to be notified of state changes.
+	 * @param {Function} callback - The callback function to be called on state changes.
+	 * @returns {Function} A function to unsubscribe the callback.
+	 */
+	subscribe(
+		callback
+	) {
+		this.subscribers.push(
+			callback
+		);
+		return () => {
+			this.subscribers =
+				this.subscribers.filter(
+					(
+						subscriber
+					) =>
+						subscriber !==
+						callback
+				);
+		};
+	}
+
+	/**
+	 * Notify all subscribers of a state change.
+	 */
+	notifySubscribers() {
+		this.subscribers.forEach(
+			(
+				subscriber
+			) =>
+				subscriber(
+					this.getState()
+				)
+		);
+	}
+
+	/**
+	 * Set the reducer function to handle state updates.
+	 * @param {Function} reducer - The reducer function.
+	 */
+	setReducer(
+		reducer
+	) {
+		this.reducer =
+			reducer;
+	}
+}
+
+// Create a new instance of the store with initial state and reducer
+const counterStore =
+	new Store({
+		count: 0,
+	});
+
+// Define a reducer function
+const counterReducer =
+	(
+		state,
+		action
+	) => {
+		switch (
+			action.type
+		) {
+			case 'INCREMENT':
+				return {
+					...state,
+					count:
+						state.count +
+						1,
+				};
+			case 'DECREMENT':
+				return {
+					...state,
+					count:
+						Math.max(
+							0,
+							state.count -
+								1
+						),
+				};
+			case 'RESET':
+				return {
+					...state,
+					count: 0,
+				};
+			default:
+				return state;
+		}
+	};
+
+// Set the reducer for the store
+counterStore.setReducer(
+	counterReducer
+);
+
+// Select DOM elements
+const plusButton =
+	document.getElementById(
+		'plus'
+	);
+const minusButton =
+	document.getElementById(
+		'minus'
+	);
+const resetButton =
+	document.getElementById(
+		'reset'
+	);
+const counterElement =
+	document.getElementById(
+		'counter'
+	);
+
+// Function to update the UI based on the state
+const updateUI = (
+	state
+) => {
+	counterElement.textContent =
+		state.count;
+};
+
+// Subscribe the updateUI function to the store
+const unsubscribe =
+	counterStore.subscribe(
+		updateUI
+	);
+
+// Attach event listeners to buttons
+plusButton.addEventListener(
+	'click',
+	() => {
+		counterStore.dispatch(
+			{
+				type: 'INCREMENT',
+			}
+		);
+	}
+);
+
+minusButton.addEventListener(
+	'click',
+	() => {
+		counterStore.dispatch(
+			{
+				type: 'DECREMENT',
+			}
+		);
+	}
+);
+
+resetButton.addEventListener(
+	'click',
+	() => {
+		counterStore.dispatch(
+			{
+				type: 'RESET',
+			}
+		);
+	}
+);
+
+// Initial UI update
+updateUI(
+	counterStore.getState()
+);
